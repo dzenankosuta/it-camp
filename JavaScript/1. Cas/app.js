19dc15168f734172d347daee467b170b863005ff
Dzenan Kosuta <dzenan_kosuta@hotmail.com>
2022-05-06 18:47:37 +0200

    Start of JavaScript

---
+++ JavaScript/1. Cas/app.js
@@ -0,0 +1,30 @@
+// alert("radii");
+
+console.log("Prvi ispis u JS konzoli");
+
+// Tipovi podataka:
+
+"nesto"                                  // string
+65                                       // number
+true, false                              // boolean
+['string', 76, true, ['string', 19]]     // niz (nizovi u JavaScript_u su nehomogeni (mogu sadrzati razlicite tipove u sebi))
+null                                     // kad nesto ne postoji
+undefined                                // nedefinisano
+{ }                                      // objekat (posebna vrsta tipova podataka (NON-PRIMITIVE))
+
+// Varijable (kontejneri za skladistenje informacija):
+
+// var ime = 'Isko';
+// console.log(ime);
+// ime = 123;
+// console.log(ime);
+
+// Operatori:
+
+console.log(2 + 2); // + je operator koji se koristi i kod brojeva i kod stringova.
+console.log(4 - 2); // - je operator koji se koristi samo kod brojeva
+console.log(2 * 3); // * je operator koji se koristi samo kod brojeva
+console.log(8 / 2); // / je operator koji se koristi samo kod brojeva
+console.log(7 % 2); // % je operator koji se koristi samo kod brojeva
+
+console.log('JavaScript ' + 'je cudan.'); // + je operator koji se koristi i kod brojeva i kod stringova.
