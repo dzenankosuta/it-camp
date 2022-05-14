075e1aaba3f9e8763ed9ee4dfa205a70c1ea0167
Dzenan Kosuta <dzenan_kosuta@hotmail.com>
2022-05-14 12:40:01 +0200

    homework

--- JavaScript/3. Cas/Domaci zadatak/fizzbuzz.js
+++ JavaScript/3. Cas/Domaci zadatak/fizzbuzz.js
@@ -1,9 +1,7 @@
 // FIZZ BUZZ CHALLENGE
 
-for (let i = 0; i <= 100; i++) {
-    if (i === 0) {
-        console.log(i);
-    } else if (i % 3 === 0 && i % 5 === 0) {
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log("FIZZ BUZZ");
     } else if (i % 3 === 0) {
         console.log("FIZZ");
