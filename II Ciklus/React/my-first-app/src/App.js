4c7d77fc2061ec0469e2fa26817b988c0e66b5e9
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-08-19 18:58:30 +0200

    generate new word and adding to array, reverse arr

--- II Ciklus/React/my-first-app/src/App.js
+++ II Ciklus/React/my-first-app/src/App.js
@@ -3,72 +3,37 @@ import "./App.css";
 import Greeting from "./components/Greeting/Greeting";
 import Incrementer from "./components/Incrementer/Incrementer";
 
-function App() {
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [hobi, setHobi] = useState("");
+const DATA = [
+  "Prva recenica",
+  "Druga recenica",
+  "Treca recenica",
+  "Cetvrta recenica",
+  "...",
+  "Stota recenica",
+];
 
-  const [userInput, setUserInput] = useState({
-    namee: "",
-    email: "",
-    hobi: "",
-  });
+function App() {
+  const [array, setArray] = useState(DATA);
+  // 1.
+  const generateNewWord = () => Math.random().toString(36).slice(2, 7);
+  // 2.
+  const reverseArray = () => {
+    const _array = [...array];
+    const reversed = _array.reverse();
+    setArray(reversed);
+  };
 
   return (
     <div className="card-container">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(userInput.namee);
-          console.log(userInput.email);
-          console.log(userInput.hobi);
-        }}
-      >
-        <label htmlFor="html">Name</label>
-        <input
-          type="text"
-          id="html"
-          name="fav_language"
-          value={userInput.namee}
-          onChange={(event) =>
-            setUserInput((prev) => ({
-              ...prev,
-              namee: event.target.value,
-            }))
-          }
-        />
-        <br />
-        <label htmlFor="css">Email</label>
-        <input
-          type="email"
-          id="css"
-          name="fav_language"
-          value={userInput.email}
-          onChange={(event) =>
-            setUserInput((prev) => ({
-              ...prev,
-              email: event.target.value,
-            }))
-          }
-        />
-        <br />
-        <label htmlFor="javascript">Hobi</label>
-        <input
-          type="text"
-          id="javascript"
-          name="fav_language"
-          value={userInput.hobi}
-          onChange={(event) =>
-            setUserInput((prev) => ({
-              ...prev,
-              hobi: event.target.value,
-            }))
-          }
-        />
-        <br />
-        <br />
-        <input type="submit" defaultValue="Submit" />
-      </form>
+      <button onClick={() => setArray((prev) => [generateNewWord(), ...prev])}>
+        Dodaj random recenicu
+      </button>
+      <button onClick={reverseArray}>REVERSE recenica</button>
+      <ul>
+        {array.map((value) => (
+          <li key={value}>{value}</li>
+        ))}
+      </ul>
     </div>
   );
 }
