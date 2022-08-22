d8548c61057f223d61662ca8262e835e38aa31f9
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-08-22 19:01:04 +0200

    Show more button

--- II Ciklus/React/my-first-app/src/components/Crypto/CryptoList.js
+++ II Ciklus/React/my-first-app/src/components/Crypto/CryptoList.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CryptoList.css";
 
 const CryptoList = ({ name, value, deleteCrypto }) => {
+  const [showMore, setShowMore] = useState();
   return (
-    <div className="crypto-list">
-      <p>{name}</p>
-      <p>$ {value}</p>
-      <button className="btn2" onClick={deleteCrypto}>
-        X
-      </button>
+    <div>
+      <div className="crypto-list">
+        <p>{name}</p>
+        <p>$ {value}</p>
+        <button className="btn2" onClick={() => setShowMore((prev) => !prev)}>
+          {!showMore ? "Show More" : "Show less"}
+        </button>
+        <button className="btn2" onClick={deleteCrypto}>
+          X
+        </button>
+      </div>
+      {showMore && (
+        <div>
+          <p>More information about {name}</p>
+        </div>
+      )}
     </div>
   );
 };
