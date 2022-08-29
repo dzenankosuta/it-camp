a16c654a1592a1dbd18dfe7d1d8ecc044266b2cf
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-08-29 18:48:44 +0200

    axios

--- II Ciklus/React/my-first-app/src/App.js
+++ II Ciklus/React/my-first-app/src/App.js
@@ -1,8 +1,45 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+const BASE_URL = "https://api.quotable.io";
+
 function App() {
-  return <div className="card-container"></div>;
+  const [authors, setAuthors] = useState([]);
+
+  function getQuote() {
+    axios.get(`${BASE_URL}/random`).then((res) => console.log(res.data));
+  }
+  function getAuthors() {
+    try {
+      axios
+        .get(`${BASE_URL}/authors?sortBy=quoteCount`)
+        .then((res) => setAuthors(res.data.results));
+    } catch (e) {
+      alert(e);
+    }
+  }
+  useEffect(() => {
+    getAuthors();
+  }, []);
+  return (
+    <div className="card-container">
+      {authors.length > 0 ? (
+        authors.map((author) => (
+          <div>
+            {author._id}
+            <h4>{author.name}</h4>
+            <h5>{author.description}</h5>
+            <p>{author.bio}</p>
+            <p>Quote count: {author.quoteCount}</p>
+            <hr />
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
