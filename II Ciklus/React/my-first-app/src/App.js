173649ffe7afec9448c2d7e00e60f8a0e624a38c
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-09-05 19:09:44 +0200

    Adding dark theme

--- II Ciklus/React/my-first-app/src/App.js
+++ II Ciklus/React/my-first-app/src/App.js
@@ -4,22 +4,34 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import CheckboxLabels from "./components/Checkbox/CheckboxLabels";
 import BasicTextFields from "./components/TextField/BasicTextFields";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 // Material UI
 function App() {
   return (
-    <div>
-      <ButtonGroup
-        variant="contained"
-        aria-label="outlined primary button group"
-      >
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main>
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
 
-      <CheckboxLabels />
-      <BasicTextFields />
-    </div>
+        <CheckboxLabels />
+        <BasicTextFields />
+      </main>
+    </ThemeProvider>
   );
 }
 
