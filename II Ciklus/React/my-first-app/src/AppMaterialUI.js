1a08f7cde27cdba4bedbe2876ba0f23b6090778d
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-09-07 18:15:50 +0200

    App from previous class - Material UI

---
+++ II Ciklus/React/my-first-app/src/AppMaterialUI.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import CheckboxLabels from "./components/Checkbox/CheckboxLabels";
+import BasicTextFields from "./components/TextField/BasicTextFields";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+// Material UI
+function AppMaterialUI() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main>
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+
+        <CheckboxLabels />
+        <BasicTextFields />
+      </main>
+    </ThemeProvider>
+  );
+}
+
+export default AppMaterialUI;
