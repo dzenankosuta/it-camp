46644bc4f3a5aa9d7613cea1ca1dbce56b89de31
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-09-09 19:45:50 +0200

    React router

--- II Ciklus/React/my-first-app/src/App.js
+++ II Ciklus/React/my-first-app/src/App.js
@@ -1,37 +1,30 @@
 import "./App.css";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import CheckboxLabels from "./components/Checkbox/CheckboxLabels";
-import BasicTextFields from "./components/TextField/BasicTextFields";
-
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import HomePage from "./components/pages/HomePage/HomePage";
+import TeamPage from "./components/pages/TeamPage/TeamPage";
+import { useNavigate } from "react-router-dom";
 
 // SPA - Single Page Application
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <main>
-        <ButtonGroup
-          variant="contained"
-          aria-label="outlined primary button group"
-        >
-          <Button>One</Button>
-          <Button>Two</Button>
-          <Button>Three</Button>
-        </ButtonGroup>
-
-        <CheckboxLabels />
-        <BasicTextFields />
-      </main>
-    </ThemeProvider>
+    <div className="card-container">
+      <h1>Welcome to React Router!</h1>
+      <Box sx={{ width: 500 }}>
+        <BottomNavigation>
+          <BottomNavigationAction label="Home" />
+          <BottomNavigationAction label="Favorites" />
+          <BottomNavigationAction label="Team" />
+        </BottomNavigation>
+      </Box>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<h1>About page</h1>} />
+        <Route path="team" element={<TeamPage />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ II Ciklus/React/my-first-app/src/components/pages/TeamPage/TeamPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeamPage = () => {
+  return <h1>Ovo je Team page</h1>;
+};
+
+export default TeamPage;
